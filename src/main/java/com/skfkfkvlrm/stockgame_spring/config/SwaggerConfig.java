be20e975009d19a6883f891b39ea3c96e713b9f9
천사무엘 <skmfmfvlrm@gmail.com>
2026-07-10 17:01:31 +0900

    feat(api): Swagger/OpenAPI 연동 및 JWT 인증 설정 추가

---
+++ src/main/java/com/skfkfkvlrm/stockgame_spring/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.skfkfkvlrm.stockgame_spring.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        String jwtSchemeName = "jwtAuth";
+        
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("STKGAME API Document")
+                .description("학생 모의투자 플랫폼 (StockGame) API 명세서")
+                .version("1.0.0");
+    }
+}
